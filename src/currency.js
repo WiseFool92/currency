325d20eddd8ce0549b40ae3c61102068ca96e092
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-24 11:56:00 -0700

    console logging to determine outputs from the API I am querying & why I keep getting a 404/false return & trying to determine what different information I might need from postman

--- src/currency.js
+++ src/currency.js
@@ -1,8 +1,10 @@
 export class Currency {
   async getCurrency() {
     try {
-      let response = await fetch(`https://cors-anywhere.herokuapp.com/https://prime.exchangerate-api.com/v5/0ddb9b5538b69fc37f389c8b/latest/USD&appid=${process.env.API_KEY}`);
+      let response = await fetch(`https://cors-anywhere.herokuapp.com/https://prime.exchangerate-api.com/v5/latest/USD&appid=${process.env.API_KEY}`);
+      console.log(response)
       let jsonifiedResponse;
+      console.log(response)
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
       } else {
