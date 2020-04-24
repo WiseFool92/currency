3d82099b05f4c540c058af62bd7f56f155e3b6bc
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-24 09:04:57 -0700

    async await func

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
+import { Currency } from './currency'
 import $ from 'jquery';
 import './styles.css';
 
 
 $(document).ready(function() {
-  $('#form').submit(function(event) {
+  $('#btnLocation').click(function(event) {
     event.preventDefault();
-   
+   (async () => {
+     let usd = new Usd();
+     const response = await usd.getCurrency();
+     getElements(response);
+   })();
+
+   function getElements(response) {
+     if (response) {
+       $('.showOutput').html('${response}');
+     } else {
+       $('.showOutput').html('Apologies there was an error trying to display your exchange.');
+     }
+   }
 });
