0df5057bced6b138156155deedf123bc4da42661
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-24 09:53:19 -0700

    fixed async await func, temp literals are now functional

--- src/main.js
+++ src/main.js
@@ -4,19 +4,20 @@ import './styles.css';
 
 
 $(document).ready(function() {
-  $('#btnLocation').click(function(event) {
-    event.preventDefault();
-   (async () => {
-     let usd = new Usd();
-     const response = await usd.getCurrency();
-     getElements(response);
-   })();
+  $('#btnLocation').click(function() {
 
-   function getElements(response) {
-     if (response) {
-       $('.showOutput').html('${response}');
-     } else {
-       $('.showOutput').html('Apologies there was an error trying to display your exchange.');
-     }
-   }
+    (async () => {
+      let usd = new Usd();
+      const response = await usd.getCurrency();
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) { 
+        $('.showOutput').html(`${response}`);
+      } else {
+        $('.showOutput').html(`Apologies there was an error trying to display your exchange.`);
+      }
+    }
+  });
 });
