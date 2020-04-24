9f53dc62e7671dfa72540cfe5af8067672b5886d
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-24 15:52:03 -0700

    console logging main js trying to determine why my if statement keeps defaulting to Conversion: Error & undefined while I am getting successful responses. modified index titles & placeholder text to be more explicit

--- src/main.js
+++ src/main.js
@@ -17,16 +17,16 @@ $(document).ready(function() {
     })();
 
     function currencyChange(response) {
-      // console.log(response)
       if (response.result === undefined) { 
         $('.showOutput').html(`<p>API Error: Check URL</p>${response}`);
       } else if (response.result === "error") {
         $('.showOutput').html(`<p>API Error: Check Key</p>${response.error}`);
       } else if (response.result === "success" && response.conversion_rates[quid]) {
         let globalExchange = response.conversion_rates[quid];
-        let convert = (type * globalExchange).toFixed(3);
+        let convert = (type * globalExchange).toFixed(2);
         $(".showOutput").html(convert + " " + quid);
       } else if (response.result === "success" && !response.conversion_rates[quid]) {
+        console.log(response.result)
         $(".showOutput").html(`<p>Conversion Error: Check if Conversion is Supported</p>${response.error}`);
       } else {
         $(".showOutput").html(`<p>Unexpected Error: We appreciate your patience. Please submit a ticket and our teams will work to resolve this issue</p>`);
