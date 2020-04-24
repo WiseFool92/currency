3247f74b64067cfc59f708765800048914c2dd79
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-24 12:36:45 -0700

    API call functioning as intended, log returned undefined, servers were timing out before giving me a false sense of failure

--- src/currency.js
+++ src/currency.js
@@ -1,10 +1,10 @@
 export class Currency {
   async getCurrency() {
     try {
-      let response = await fetch(`https://cors-anywhere.herokuapp.com/https://prime.exchangerate-api.com/v5/latest/USD&appid=${process.env.API_KEY}`);
-      console.log(response)
+      let response = await fetch(`https://cors-anywhere.herokuapp.com/https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+      // console.log(response)
       let jsonifiedResponse;
-      console.log(response)
+      // console.log(response)
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
       } else {
