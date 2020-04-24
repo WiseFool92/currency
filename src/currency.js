971d8b183ba16521b9d80ee5101da7deda78c2ba
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-24 10:07:18 -0700

    template literal functioning

--- src/currency.js
+++ src/currency.js
@@ -1,7 +1,7 @@
 export class Currency {
   async getCurrency() {
     try {
-      let response = await fetch('https://cors-anywhere.herokuapp.com/https://prime.exchangerate-api.com/v5/0ddb9b5538b69fc37f389c8b/latest/USD&appid=${process.env.API_KEY}');
+      let response = await fetch(`https://cors-anywhere.herokuapp.com/https://prime.exchangerate-api.com/v5/0ddb9b5538b69fc37f389c8b/latest/USD&appid=${process.env.API_KEY}`);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
@@ -9,7 +9,7 @@ export class Currency {
         jsonifiedResponse = false;
       }
       return jsonifiedResponse;
-    } catch (error) {
+    } catch(error) {
       return false;
     }
   }
